81a04da463f2edd1dd3c18c45c6bbeb6730eb2c4
Bhoomil Dayani <bhoomildayani182@gmail.com>
2022-09-16 10:14:03 +0530

    Improved Variable Names and Schema

--- backend/db/models/class.js
+++ backend/db/models/class.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 //Defining the class schema for class Model
 const classSchema = mongoose.Schema({
   //Defining the Class ID property on class Schema
-  classID: {
+  className: {
     type: String,
     required: true,
     trim: true,
@@ -18,13 +18,13 @@ const classSchema = mongoose.Schema({
   },
 
   //Defining the session property on class Schema
-  section: {
+  session: {
     type: String,
     required: true,
     trim: true,
   },
 });
 
-const Subject = mongoose.model("Subject", subjectSchema);
+const Class = mongoose.model("Class", classSchema);
 
-module.exports = Subject;
+module.exports = Class;

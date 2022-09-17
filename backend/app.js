e08de1e8240dd74ed71beb3de318551a49429d3d
Bhoomil Dayani <bhoomildayani182@gmail.com>
2022-09-17 19:10:46 +0530

    Added: Routes for Subject deletion

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-require("./db/mongoose");
+require('./db/mongoose');
 
-const userRouter = require("./routes/user");
-const subjectRouter = require("./routes/subject");
-const classRouter = require("./routes/class");
+const userRouter = require('./routes/user');
+const subjectRouter = require('./routes/subject');
+
 
 app.use(express.json());
 app.use(userRouter);
 app.use(subjectRouter);
-app.use(classRouter);
 
 // PORT
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log("Server is up on port" + port);
-});
+app.listen(port, ()=>{
+    console.log('Server is up on port' + port);
+})

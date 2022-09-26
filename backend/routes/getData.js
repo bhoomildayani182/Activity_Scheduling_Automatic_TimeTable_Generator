61f7ac50a7d78a458b6d7b62dd1f8518e43e757b
Bhoomil Dayani <bhoomildayani182@gmail.com>
2022-09-26 23:12:53 +0530

    Fixed Export syntax, Route, mongod Script, a typo, data manipulation

--- backend/routes/getData.js
+++ backend/routes/getData.js
@@ -5,52 +5,61 @@ const Subject = require("../db/models/subject");
 const Teacher = require("../db/models/teacher");
 const Slots = require("../db/models/addSlots");
 const TimeTable = require("../src/timetable/timetable");
-
 const router = express.Router();
-const sectionArray = [];
-const subjectArray = [];
-const teacherArray = [];
-const slotsArray = [];
-const slotsObj = {};
-const givenSlots = [9, 9, 9, 9, 9];
 
 router.get("/api/fetchdata", auth, async (req, res) => {
+  let sectionArray = [];
+  let subjectArray = [];
+  let teacherArray = [];
+  let slotsArray = [];
+  let slotsObj = {};
+  let givenSlots = [9, 9, 9, 9, 9];
   try {
-    const sectionData = await Section.find({ author: req.user._id });
-    const subjectData = await Subject.find({ author: req.user._id });
-    const teacherData = await Teacher.find({ author: req.user._id });
-    const slotsData = await Slots.find({ author: req.user._id });
-    sectionData.forEach((cur) => {
-      sectionArray[cur] = [slotsData[cur.session]+"-"+slotsData[cur.section]];
-    });
-    subjectData.forEach((cur) => {
-      subjectArray[cur] = subjectData[cur.subjectName];
-    });
-    teacherData.forEach((cur) => {
-      teacherArray[cur] = teacherData[cur.firstName + " " + cur.lastName];
-    });
-    slotsData.forEach((cur) => {
-      slotsObj.teacher = slotsData[cur.teacherName];
-      slotsObj.sections = [slotsData[cur.session]+"-"+slotsData[cur.section]];
-      slotsObj.subject = slotsData[cur.subjectName];
-      slotsObj.numLectures = slotsData[cur.contactHours];
+    let sectionData = await Section.find({ author: req.user._id });
+    let subjectData = await Subject.find({ author: req.user._id });
+    let teacherData = await Teacher.find({ author: req.user._id });
+    let slotsData = await Slots.find({ author: req.user._id });
+    //console.log(slotsData);
+
+    for (let i = 0; i < sectionData.length; i++) {
+      sectionArray[i] = `${sectionData[i].session}-${sectionData[i].section}`;
+    }
+
+    for (let i = 0; i < subjectData.length; i++) {
+      subjectArray[i] = `${subjectData[i.subjectName]}`;
+    }
+    //console.log(subjectArray);
+
+    for (let i = 0; i < teacherData.length; i++) {
+      teacherArray[i] = `${teacherData[i.firstName]} ${
+        teacherData[i.lastName]
+      }`;
+    }
+    //console.log(teacherArray);
+
+    for (let j = 0; j < slotsData.length; j++) {
+      slotsObj.teacher = `${slotsData[j.teacherName]}`;
+      slotsObj.sections = `[${slotsData[j.session]}-${slotsData[j.section]}]`;
+      slotsObj.subject = `${slotsData[j.subjectName]}`;
+      slotsObj.numLectures = `${slotsData[j.contactHours]}`;
       for (let i = 0; i < subjectData.length; i++) {
-        if (slotsData[cur.subjectName] === subjectData[i.subjectName]) {
-          slotsObj.labs = subjectData[i.labs];
+        if (`${slotsData[j.subjectName]}` === `${subjectData[i.subjectName]}`) {
+          slotsObj.labs = `${subjectData[i.labs]}`;
           break;
         }
       }
-      slotsArray[cur] = slotsObj;
-    });
+      slotsArray[j] = slotsObj;
+    }
+    //console.log(slotsData);
 
-    const resultedTimeTable = await TimeTable(
+    const resultedTimeTable = TimeTable(
       slotsArray,
       givenSlots,
       teacherArray,
       sectionArray
     );
 
-    res.status(200).send(resultedTimeTable);
+    res.status(200).send("resultedTimeTable");
   } catch (err) {
     res.status(400).send(err);
   }
